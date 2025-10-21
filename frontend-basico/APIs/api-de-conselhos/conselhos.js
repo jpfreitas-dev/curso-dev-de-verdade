794916f5b14c87e1f7831a2953038e70ab92f6d5
João Paulo Freitas Pinho <joaopaulofreitasp23@gmail.com>
2025-10-21 19:55:09 -0300

    refactor: melhora a logica da requisicao api e adiciona comentarios

--- frontend-basico/APIs/api-de-conselhos/conselhos.js
+++ frontend-basico/APIs/api-de-conselhos/conselhos.js
@@ -1,22 +1,30 @@
 const adviceText = document.getElementById('advice-text');
 const adviceNumber = document.getElementById('advice-number');
-const diceButton = document.querySelector('.dice-button');
+const diceButton = document.querySelector('.dice-button'); 
 
 let isLoading = false;
 
-diceButton.addEventListener('click', async function () {
-    diceButton.classList.add('dice-button-animation');
-    if (isLoading) return;
+async function fetchAdvice(){
+    if (isLoading) return; // Verifica se já está carregando um conselho
 
-    isLoading = true;
+    isLoading = true; // Evita múltiplas requisições simultâneas
     const response = await fetch('https://api.adviceslip.com/advice');
     const advice = await response.json();
 
     adviceText.textContent = advice.slip.advice;
     adviceNumber.textContent = '#'+advice.slip.id;
 
-    isLoading = false;
+    isLoading = false; // Permite novas requisições
+}
+
+diceButton.addEventListener('click', async function () {
+    diceButton.classList.add('dice-button-animation');
+
+    fetchAdvice();
+
     setTimeout(() => {
         diceButton.classList.remove('dice-button-animation')
     }, 1000);
 });
+
+fetchAdvice(); // Carrega um conselho ao iniciar a página
